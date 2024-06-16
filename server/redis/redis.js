2bfba5b6b81247b4ab7fa9d7ac3cb7493b68cbf4
Varshil Shah <varshilshah.in@gmail.com>
2024-06-16 20:15:49 +0530

    Define utilify functions for redis

---
+++ server/redis/redis.js
@@ -0,0 +1,81 @@
+const Redis = require("ioredis");
+const { REDIS_KEYS } = require("../constants");
+
+const redis = new Redis(process.env.REDIS_URL);
+
+async function enqueJobInQueue(job) {
+  return await redis.lpush(
+    REDIS_KEYS.VIDEO_TRANSCODING_QUEUE,
+    JSON.stringify(job)
+  );
+}
+
+async function dequeueJobFromQueue() {
+  const job = await redis.rpop(REDIS_KEYS.VIDEO_TRANSCODING_QUEUE);
+  return job ? JSON.parse(job) : null;
+}
+
+async function getKey(key) {
+  return await redis.get(key);
+}
+
+async function setKey(key, value, options = {}) {
+  if (!redis) {
+    console.log("Redis connection not established");
+    return;
+  }
+
+  try {
+    const defaultOptions = {
+      expire: 0,
+      setIfNotExist: false,
+    };
+    const mergedOptions = { ...defaultOptions, ...options };
+
+    const params = [key, value];
+    if (mergedOptions.expire > 0) {
+      params.push("EX", mergedOptions.expire);
+    }
+    if (mergedOptions.setIfNotExist) {
+      params.push("NX");
+    }
+
+    const response = await redis.send_command("SET", params);
+
+    if (response === "OK") {
+      console.log(`${key} set to ${value}`);
+      return true;
+    } else {
+      throw new Error("Failed to set key");
+    }
+  } catch (error) {
+    console.error(`Error setting key ${key}:`, error);
+    return false;
+  }
+}
+
+async function deleteKey(key) {
+  return await redis.del(key);
+}
+
+async function increment(key) {
+  const value = await redis.incr(key);
+  console.log("Incremented value: ", value);
+  return value;
+}
+
+async function decrement(key) {
+  const value = await redis.decr(key);
+  console.log("Decremented value: ", value);
+  return value;
+}
+
+module.exports = {
+  enqueJobInQueue,
+  dequeueJobFromQueue,
+  getKey,
+  setKey,
+  deleteKey,
+  increment,
+  decrement,
+};
--- server/utils/s3-signed-url.js
+++ server/utils/s3-signed-url.js
@@ -30,6 +30,7 @@ async function generateUrlToPutObject(userId, fileName, contentType) {
       expiresIn: 3600,
     });
 
+    console.log("Signed url generated to put object");
     return signedUrl;
   } catch (error) {
     console.log("Error occured while generating signed URL to put object");
@@ -37,4 +38,19 @@ async function generateUrlToPutObject(userId, fileName, contentType) {
   }
 }
 
-module.exports = { generateUrlToPutObject };
+async function deleteObjectFile(key) {
+  try {
+    const command = new DeleteObjectCommand({
+      Bucket: process.env.TEMP_S3_BUCKET_NAME,
+      Key: key,
+    });
+
+    await s3Client.send(command);
+    console.log("Object deleted successfully");
+  } catch (error) {
+    console.log("Error occured while deleting object");
+    console.log(error);
+  }
+}
+
+module.exports = { generateUrlToPutObject, deleteObjectFile };
